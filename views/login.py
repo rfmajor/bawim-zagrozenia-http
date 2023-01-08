05407fd9f374bea4999968e1e4643afe734271ba
rfmajor <rfmajor99@gmail.com>
2023-01-08 14:53:31 +0100

    Add SQL init script, configure database

--- views/login.py
+++ views/login.py
@@ -1,8 +1,6 @@
-import MySQLdb.cursors
 from flask import Blueprint, redirect, url_for
 from flask import render_template, request, session
-
-from app import db
+from app import get_db_connection
 
 bp = Blueprint('login', __name__)
 
@@ -13,15 +11,17 @@ def login():
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM users WHERE username = % s AND password = % s', (username, password,))
-        account = cursor.fetchone()
-        if account:
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            msg = 'Logged in successfully !'
-            return render_template('home.html', msg=msg)
-        else:
-            msg = 'Incorrect username / password !'
-    return render_template('login.html', msg=msg, title="Sign In")
+
+        connection = get_db_connection()
+        with connection:
+            cursor = connection.cursor()
+            cursor.execute('SELECT * FROM users WHERE username = ?;', (username,))
+            account = cursor.fetchone()
+            if account and account['password'] == password:
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                msg = 'Logged in successfully !'
+            else:
+                msg = 'Incorrect username / password !'
+    return render_template('login.html', msg=msg)
